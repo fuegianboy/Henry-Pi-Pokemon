8391c6041f008a7d96158f3961e3cc499252a885
fuegianboy <fuegianboy@hotmail.com>
2023-10-21 21:18:17 -0300

    21102023 2118

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "./About.module.css";
+import photo from "../img/AdrianVelazquez.JPG"
 
 const About = () => {
   return (
-    <div className={style.divContainer}>
-      <div>
-        <Link to="/home">
-            <h3 >Home</h3>
-            <div  className={style.pokeball}></div>
-        </Link>
+    <div>
         <div className={style.divContainer}>
-          <div className={style.divPhoto}>
-            <img></img>
+          <div>
+            <Link to="/home">
+                <h3 className={style.h3}>Home</h3>
+                <div  className={style.pokeball}></div>
+            </Link>
           </div>
-          <div className={style.divContacts}>
 
-          </div>
-          <div className={style.divTech}>
+          <div className={style.divData}>
 
+            <h6>Adrian Velazquez</h6>
+            <img src={photo} alt="Not found" width={"150px"}/>
+            <h6>Email: </h6>
+            <h6>a.velazquez.tf@gmail.com</h6>
+            <h6>GitHub:</h6>
+            <h6>https://github.com/fuegianboy</h6>
+            <h6>LinkedIn</h6>
+            <h6>https://www.linkedin.com/in/adrian-velazquez-182a242/</h6>
           </div>
-          <h3>aca van mis datos</h3>
         </div>
-      </div>
     </div>
   );
 };
